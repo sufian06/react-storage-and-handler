9de05f34b9d6b8ab1bbc60677d47837f985bc564
sufian <sufian4se@gmail.com>
2022-09-24 17:20:43 +0600

    just one item data storage to localStorage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,22 +1,19 @@
 import React from "react";
+import { addToDB } from "../utilities/fakedb";
 import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
   const { name, age, id } = props.cosmetic;
   const addToCart = (id) => {
-    console.log('item added', id)
+    addToDB(id)
   }
-
-  const addToCartWithParam = () => addToCart(id)
 
   return (
     <div className="product">
       <h2>Buy this: {name}</h2>
       <p>Age: {age} years</p>
       <p><small>it has id: {id}</small></p>
-      <button onClick={addToCartWithParam}>Add to Cart</button>
-      <button onClick={() => addToCart(id)}>Add to Cart: shortcut</button>
-      <button onClick={() => addToCart(id)}>Purchase</button>
+      <button onClick={() => addToCart(id)}>Add to Cart</button>
     </div>
   );
 };
